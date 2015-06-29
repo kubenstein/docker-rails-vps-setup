72875c5f8e0068c334f7bf30ea6d4b35048bb221
Jakub Niewczas <niewczas.jakub@gmail.com>
2015-06-29 15:03:57 +0200

    refactor configurate.sh

--- bootstrap.zip/setup-contaiers-configuration/configurate.sh
+++ bootstrap.zip/setup-contaiers-configuration/configurate.sh
@@ -4,32 +4,82 @@
 # It will create all basic containers on the host mashine
 #
 
-# create db volume container
-echo "[CONFIGURATOR] setup db volume container"
-docker run -d --name db-storage \
-              postgres /bin/true
+function main() {
+  build_custom_images
+  setup_db_volume_container
+  setup_db_container
+  setup_git_receiver_storage
+  setup_git_receiver
+}
 
 
-# create db container
-echo "[CONFIGURATOR] setup db container"
-docker run -d --name db \
-              -p 5432:5432 \
-              --volumes-from db-storage \
-              postgres
+function build_custom_images() {
+  docker build -t git-receiver ./image-files/git-receiver
+}
 
 
-# create git keys storage
-echo "[CONFIGURATOR] setup git receiver storage"
-docker build -t git-receiver ./image-files/git-receiver
+function setup_db_volume_container() {
+  echo -e "\n* [CONFIGURATOR] setup db volume container"
+  if container_exists "db-storage"; then return; fi
 
-docker run -d --name git-receiver-storage \
-              git-receiver /bin/true
+  docker run -d --name db-storage \
+             postgres /bin/true
+}
 
 
-echo "[CONFIGURATOR] setup git receiver"
-docker run -d --name git-receiver \
-              -p 2222:22 \
-              --volumes-from git-receiver-storage \
-              -v /var/run/docker.sock:/var/run/docker.sock \
-              -e "GIT_USER_PASSWORD=$GIT_USER_PASSWORD" \
-              git-receiver
+function setup_db_container() {
+  echo -e "\n* [CONFIGURATOR] setup db container"
+  if container_exists "db"; then 
+    stop_and_remove_container db
+  fi
+
+  docker run -d --name db \
+                -p 5432:5432 \
+                --volumes-from db-storage \
+                postgres
+}
+
+
+function setup_git_receiver_storage() {
+  echo -e "\n* [CONFIGURATOR] setup git receiver storage"
+  if container_exists "db-storage"; then return; fi
+
+  docker run -d --name git-receiver-storage \
+                git-receiver /bin/true
+}
+
+
+function setup_git_receiver() {
+  echo -e "\n* [CONFIGURATOR] setup git receiver"
+  if container_exists "git-receiver"; then 
+    stop_and_remove_container git-receiver
+  fi
+
+  docker run -d --name git-receiver \
+                -p 2222:22 \
+                --volumes-from git-receiver-storage \
+                -v /var/run/docker.sock:/var/run/docker.sock \
+                -e "GIT_USER_PASSWORD=$GIT_USER_PASSWORD" \
+                git-receiver
+}
+
+
+# private 
+function stop_and_remove_container() {
+  CONTAINER_NAME=$1
+
+  # todo add some checks if container is running
+  # to prevent displaying errors
+  docker rm -f -v $CONTAINER_NAME
+}
+
+function container_exists() {
+  ID=$(docker inspect --format="{{ .Id }}" $1 2> /dev/null)
+  if [ -n "$ID" ]; then
+    return 0; # 0 = true
+  fi
+  return 1; # 1 = false
+}
+
+# run!
+main
