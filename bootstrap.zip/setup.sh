12d1e048c423c1021c5c09f3c574d223a3f34277
Jakub Niewczas <niewczas.jakub@gmail.com>
2015-07-02 10:19:12 +0200

    conditional install docker and add non-root user

--- bootstrap.zip/setup.sh
+++ bootstrap.zip/setup.sh
@@ -19,17 +19,21 @@ read GIT_USER_PASSWORD
 function main() {
   add_non_root_user_on_host
   add_docker_user_on_host
-  install_docker
+  install_docker_if_needed
   setup_basic_containers
 }
 
 
 function add_non_root_user_on_host() {
-  echo "*********** add non-root user ***********"
-  echo "username: "
-  read NON_ROOT_USER
-  sudo adduser --gecos "" $NON_ROOT_USER
-  sudo adduser $NON_ROOT_USER sudo
+  echo "*********** add non-root user? ***********"
+  echo "y/n"
+  read ADD_NON_ROOT_USER
+  if [ "$ADD_NON_ROOT_USER" == "y" ]; then
+    echo "username:"
+    read NON_ROOT_USER
+    sudo adduser --gecos "" $NON_ROOT_USER
+    sudo adduser $NON_ROOT_USER sudo
+  fi
 }
 
 
@@ -39,11 +43,13 @@ function add_docker_user_on_host() {
 }
 
 
-function install_docker() {
+function install_docker_if_needed() {
   echo "*********** install docker ***********"
-  sudo apt-get install -qq -y wget
-  sudo sh -c "wget -qO- https://get.docker.com/ | sh"
-
+  DOCKER_INSTALLED=$(sudo initctl list | grep 'docker start/running' | wc -l)
+  if [ $DOCKER_INSTALLED -eq 0 ]; then
+    sudo apt-get install -qq -y wget
+    sudo sh -c "wget -qO- https://get.docker.com/ | sh"
+  fi
   sudo gpasswd -a docker-host docker
   sudo service docker restart
 }
