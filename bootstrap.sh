62edf8e3632c9c9d53e165af483dea911ad6b687
Jakub Niewczas <niewczas.jakub@gmail.com>
2015-07-07 10:09:23 +0200

    fix missing bootstrap files after scp

--- bootstrap.sh
+++ bootstrap.sh
@@ -34,7 +34,7 @@ function copy_files() {
 
 function initialize_process() {
   echo "* ssh to $HOST_IP to initialize setup"
-  ssh -t $HOST_ROOT_USER@$HOST_IP 'sudo sh -c "cp -R ~/bootstrap.zip /usr/shared/ && sleep 5 && /usr/shared/bootstrap.zip/setup.sh"'
+  ssh -t $HOST_ROOT_USER@$HOST_IP 'sudo sh -c "mkdir -p /usr/shared/bootstrap.zip/ && cp -r ~/bootstrap.zip/ /usr/shared/ && /usr/shared/bootstrap.zip/setup.sh"'
 }
 
 
